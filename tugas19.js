43b1cfc02968ae2dddf2add45f848dbb92639e33
Adam <syahnetwork@gmail.com>
2020-04-17 22:00:59 +0700

    tugas19

---
+++ tugas19.js
@@ -0,0 +1,13 @@
+let sentence1 = 'Semangat...'
+
+console.log(sentence1.repeat(5))
+
+let sentence2 = "*"
+
+console.log(sentence2.repeat(5));
+console.log(sentence2.repeat(20));
+console.log(sentence2.repeat(50));
+console.log(sentence2.repeat(100));
+console.log(sentence2.repeat(50));
+console.log(sentence2.repeat(20));
+console.log(sentence2.repeat(5));
