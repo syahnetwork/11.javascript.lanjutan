1010c600102a393a61d36359bd31d1f000f1e39d
Adam <syahnetwork@gmail.com>
2020-04-17 22:14:54 +0700

    tugas20

---
+++ 35converttostring.js
@@ -0,0 +1,16 @@
+convert = () => {
+  let sen1 = new String('belajar kuy..')
+
+  console.log(typeof sen1)
+  console.log(sen1)
+
+  let sen2 = sen1.toString()
+  console.log(typeof sen2)
+  console.log(sen2)
+
+  let sen3 = sen1.valueOf()
+  console.log(typeof sen3)
+  console.log(sen3)
+}
+
+convert()
---
+++ tugas20.js
@@ -0,0 +1,5 @@
+let sen1 = 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable.'
+
+console.log(sen1.match('are'))
+console.log(sen1.match('borat'))
+console.log(sen1.match('words'))
