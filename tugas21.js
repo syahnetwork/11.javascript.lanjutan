7ed4e797804edfa56aa2dce82dab606e93aca93a
Adam <syahnetwork@gmail.com>
2020-04-17 22:31:34 +0700

    tugas21

---
+++ tugas21.js
@@ -0,0 +1,10 @@
+stringReplace = () => {
+  let sen1 = 'Saya belajar masak nasi goreng'
+
+  let sen2 = sen1.replace('masak', 'makan')
+  
+  console.log(sen1)
+  console.log(sen2)
+}
+
+stringReplace()
